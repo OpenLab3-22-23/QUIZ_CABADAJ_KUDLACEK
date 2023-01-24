93bdbbde0d52d5b63d4b5410be77ae4278df3148
z20ist12 <z20ist12@z00p3-10.SPSKNM>
2023-01-24 09:15:34 +0100

    Slovencina

--- src/Slovencina.tsx
+++ src/Slovencina.tsx
@@ -1,7 +1,36 @@
 
 
 function Slovencina() {
-  return <div>Toto je strana pre Quiz Slovenčina</div>;
+  return(
+  <>
+    <div className="button-container"></div>
+    <div className="sjl-nadpis">Quiz Slovenčina</div>
+      <div className="sjl-otazka">    
+        <h2 className="sjl-txtotazka">??????????????</h2>
+      </div>
+      <div className="sjl-odpovede">
+        
+        <div className="sjl-row1">
+          <div className="sjl-odpoved1">  
+            <h2 className="sjl-txtodpoved1">??????????????</h2>
+          </div>
+          <div className="sjl-odpoved3">  
+            <h2 className="sjl-txtodpoved3">??????????????</h2>
+          </div>
+        </div>
+        
+        <div>
+          <div className="sjl-odpoved2">  
+            <h2 className="sjl-txtodpoved2">??????????????</h2>
+          </div>
+          <div className="sjl-odpoved4">  
+            <h2 className="sjl-txtodpoved4">??????????????</h2>
+          </div>
+        </div>
+
+      </div>
+  </>
+  );
 }
 
 export default Slovencina;
