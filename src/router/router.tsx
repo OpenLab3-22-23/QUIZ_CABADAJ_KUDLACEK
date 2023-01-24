7d86202eaff06bc29054c610be64906b9da4aa1f
z20ist03 <z20ist03@z00p3-09.SPSKNM>
2023-01-24 08:04:19 +0100

    link

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,6 +2,12 @@ import { createBrowserRouter, Link } from "react-router-dom";
 import App from "../App";
 import LogIn from "../auth/Login";
 import SignUp from "../auth/SignUp";
+import Dejepis from "../Dejepis";
+import Fyzika from "../Fyzika";
+import Informatika from "../Informatika";
+import Matematika from "../Matematika";
+import Random from "../Random";
+import Slovencina from "../Slovencina";
 
 
 
@@ -18,4 +24,28 @@ export const router = createBrowserRouter([
     path: "/login",
     element: <LogIn />,
   },
+  {
+    path: "/slovencina",
+    element: <Slovencina />
+  },
+  {
+    path: "/matematika",
+    element: <Matematika />
+  },
+  {
+    path: "/informatika",
+    element: <Informatika/>
+  },
+  {
+    path: "/dejepis",
+    element: <Dejepis />
+  },
+  {
+    path: "/fyzika",
+    element: <Fyzika />
+  },
+  {
+    path: "/random ",
+    element: <Random/>
+  }
 ]);
