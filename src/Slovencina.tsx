07601d5d6e34a7631989f57529363de2999356c2
Radoslav304 <90415819+Radoslav304@users.noreply.github.com>
2023-02-02 20:21:21 +0100

    vypísovanie otázok a odpoví do appky ✔

--- src/Slovencina.tsx
+++ src/Slovencina.tsx
@@ -1,52 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import Supabase from '@supabase/supabase-js';
-import { slovencinaQuestions } from './supabase/supabaseClient';
+import { slovencinaQuestions, supabase } from './supabase/supabaseClient';
 
 
 const Slovencina = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
-  const questions = [
-    {
-      question: 'Koľko vzorov má mužský rod?',
-      options: [5, 3, 1, 4,],
-      correctAnswer: 4,
-    },
-  ];
+  const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
-    const {data, error} = slovencinaQuestions()
-  }, [])
+    supabase
+      .from('slovencina')
+      .select()
+      .then((data) => {
+        setQuestions(data.data);
+      });
+  }, []);
 
   const handleAnswer = (answer) => {
     if (answer === questions[currentQuestion].correctAnswer) {
       setScore(score + 1);
     }
     setCurrentQuestion(currentQuestion + 1);
-  }
+  };
 
   return (
     <>
-    <div className="button-container"></div>
+      <div className="button-container"></div>
       <div className="sjl-nadpis">Quiz Slovenčina</div>
 
       {currentQuestion < questions.length && (
         <>
-        <div className="sjl-otazka"> 
-          <h2 className="sjl-txtotazka">{questions[currentQuestion].question}</h2>
-        </div>
-            {questions[currentQuestion].options.map((option) => (
-              <div className="">
+          <div className="sjl-otazka">
+            <h2 className="sjl-txtotazka">{questions[currentQuestion].question}</h2>
+          </div>
+          {[questions[currentQuestion].option1, 
+            questions[currentQuestion].option2, 
+            questions[currentQuestion].option3, 
+            questions[currentQuestion].option4].map((option) => (
+            <div className="">
               <button onClick={() => handleAnswer(option)}>{option}</button>
-              </div>
-            ))}
+            </div>
+          ))}
         </>
       )}
       {currentQuestion === questions.length && <h2>Your score: {score} / {questions.length}</h2>}
     </>
   );
-}
+};
 
 export default Slovencina;
-        
-    
