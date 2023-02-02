826d3606e539cd688f18682b4842e43aa224f7d9
z20ist12 <z20ist12@z00p3-10.SPSKNM>
2023-02-02 08:55:33 +0100

    supabase get data, first

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import "./App.css";
 import { useAuth } from "./auth/Auth";
 import LandingPage from "./LandingPage";
 import Slovencina from "./Slovencina";
+import { supabase } from "./supabase/supabaseClient";
 
 function App() {
   const { session } = useAuth();
+  useEffect(()=>{
+    async function fetchData() {
+      const { data, error } = await supabase
+    .from('slovencina')
+    .select()
+
+    if (error)
+      console.log(error);
+    console.log(data)
+    }
+    
+    fetchData();
+  
+  },[])
 
   return (
     <div className="w-screen h-screen ">
