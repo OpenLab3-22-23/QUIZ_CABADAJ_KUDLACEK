22feb21a6ab94491a166fa40ec9fe6f146ddb08f
Radoslav304 <90415819+Radoslav304@users.noreply.github.com>
2023-02-12 12:12:52 +0100

    loading-rozrobený

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,6 +1,6 @@
 import openlab from "./assets/openlab.svg";
 import { useAuth } from "./auth/Auth";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Slovencina from "./Slovencina";
 import LogIn from "./auth/Login";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
@@ -11,44 +11,60 @@ import Fyzika from "./Fyzika";
 import Informatika from "./Informatika";
 
 export default function LandingPage(): JSX.Element {
-
     const {signOut} = useAuth()
+    const [loading, setLoading] = useState(false);
 
     function handleLogOut(): void {
-        signOut();
+      signOut();
+    }
+    
+    const Loading = () => (
+      <div className="loading">
+        <div className="spinner"></div>
+          <p>Načítava sa...</p>
+      </div>
+);
+
+
+    function handleButtonClick() {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
     }
 
+    useEffect(() => {
+        setLoading(false);
+    }, []);
 
     return  (
         <>
       <div className="button-container">
         <div className="button-row">
           <Link to="/slovencina">
-            <button className="btn-questions">Slovenčina</button>
+            <button className="btn-questions" onClick={handleButtonClick}>Slovenčina</button>
           </Link>
           <Link to="/matematika">
-            <button className="btn-questions">Matematika</button>
+            <button className="btn-questions" onClick={handleButtonClick}>Matematika</button>
           </Link>
           <Link to="/informatika">
-            <button className="btn-questions">Informatika</button>
+            <button className="btn-questions" onClick={handleButtonClick}>Informatika</button>
           </Link>
         </div>
         <div className="button-row">
           <Link to="/dejepis">
-            <button className="btn-questions">Dejepis</button>
+            <button className="btn-questions" onClick={handleButtonClick}>Dejepis</button>
           </Link>
           <Link to="/fyzika">
-            <button className="btn-questions">Fyzika a Biológia</button>
+            <button className="btn-questions" onClick={handleButtonClick}>Fyzika a Biológia</button>
           </Link>
           <Link to="/random">
-            <button className="btn-questions">Random otázky</button>
+            <button className="btn-questions" onClick={handleButtonClick}>Random otázky</button>
           </Link>
         </div>
       </div>
+      {loading && <Loading />}
       <button onClick={handleLogOut}>Odhlásiť sa</button>
       </>
-      
-        
     );
 };
-
