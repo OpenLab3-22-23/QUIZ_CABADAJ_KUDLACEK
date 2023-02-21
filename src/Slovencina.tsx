eea5af11d161e5f269e916c2e9e0cdf311c4adb6
z20ist12 <z20ist12@z00p3-10.SPSKNM>
2023-02-21 10:02:43 +0100

    Nedokoncene vyhodnocovanie efektom

--- src/Slovencina.tsx
+++ src/Slovencina.tsx
@@ -7,10 +7,13 @@ import QuizScore from './QuizScore';
 const Slovencina = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
-  const [questions, setQuestions] = useState([]);const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [numCorrect, setNumCorrect] = useState(0);
   const [numIncorrect, setNumIncorrect] = useState(0);
-  const [quizEnded, setQuizEnded] = useState(false);
+  const [quizEnded, setQuizEnded] = useState(false); 
+  
+  const [answer1, setAnswer1] = useState("");
 
  useEffect(() => {
     setLoading(true);
@@ -24,23 +27,38 @@ const Slovencina = () => {
         setQuestions(data.data);
       });
   }, []);
-
-  const handleAnswer = (answer) => {
+  const handleAnswer = (e, answer) => {
+    const clickedAnswer=e.target;
+    console.log(clickedAnswer);
     if (answer === questions[currentQuestion].correctAnswer) {
-      setNumCorrect(numCorrect + 1);
-      setScore(score + 1);
+      const timer = setTimeout(() => {
+        {
+            console.log("spravna odpoved");
+            setCurrentQuestion(currentQuestion + 1)
+        }
+      }, 1000);
+      {
+        setNumCorrect(numCorrect + 1);
+        setScore(score + 1);
+      }
+      return () => clearTimeout(timer);
     } else {
-      setNumIncorrect(numIncorrect + 1);
-    }
-    setCurrentQuestion(currentQuestion + 1);
-    if (currentQuestion + 1 < questions.length) {
-      setCurrentQuestion(currentQuestion + 1);
-    } else {
-      setQuizEnded(true);
+      const timer = setTimeout(() => {
+        {
+          console.log("nespravna odpoved");
+          setNumIncorrect(numIncorrect + 1);
+          setCurrentQuestion(currentQuestion + 1)
+          if (currentQuestion + 1 < questions.length) {
+          } else {
+            setQuizEnded(true);
+          }
+        }
+      }, 1000);
+      {
+        
+      }
     }
   };
-
-  
 
   return (
     <>
@@ -63,8 +81,8 @@ const Slovencina = () => {
               <div className="button-container-questions">
                 <div className="button-row">
                   <button
-                    className="quiz-odpoved1 quiz-txtodpoved1"
-                    onClick={() => handleAnswer(questions[currentQuestion].option1)}
+                    className={"quiz-odpoved1 quiz-txtodpoved1" + answer1}
+                    onClick={(e) => handleAnswer(e, questions[currentQuestion].option1)}
                   >
                     A: {questions[currentQuestion].option1}
                   </button>
